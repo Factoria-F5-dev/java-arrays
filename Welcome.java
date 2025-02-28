f3a243fc7b2fff687918c75b8660a6245cd11801
MAlexGG <alexandragalarzag@gmail.com>
2025-02-28 09:06:32 +0100

    array exercise lack of array in Welcome file

--- Welcome.java
+++ Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 
 
         //Escribe un bucle 'for' que imprima solo los siguientes números: 10 - 30 - 50 - 70 - 90 del array numbers, sin modificarlo ni creando uno nuevo
+        int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90};
 
 
         //Crea un array que contenga algunos números y usa un bucle 'for-each' e imprime cada número duplicado
